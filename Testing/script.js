e2c38383b5d01dd65999435f32440e4d2da05c6f
Spencer Pollock <srepollock@gmail.com>
2015-05-05 09:03:23 -0700

    Site Concept

--- Testing/script.js
+++ Testing/script.js
@@ -1,14 +1,32 @@
 zebra.ready(function() {
-        var canvas = new zebra.ui.zCanvas("can");
-        var toilet = new zebra.ui.ImagePan("./pics/pic2.png");
-        var play = new zebra.ui.Button(new zebra.ui.ImagePan("./pics/PlayButton.png"));
-        var inst = new zebra.ui.Button(new zebra.ui.ImagePan("./pics/InstructionsButton.png"));
-        canvas.setBorder(zebra.ui.borders.plain);
-        canvas.root.setLayout(new zebra.layout.BorderLayout(8));
-        canvas.root.add(zebra.layout.CENTER, toilet);
-        var botP = new zebra.ui.Panel(new zebra.layout.FlowLayout(
-            zebra.layout.CENTER, zebra.layout.BOTTOM, zebra.layout.VERTICAL, 2));
-        botP.add(play);
-        botP.add(inst);
-        canvas.root.add(zebra.layout.BOTTOM, botP);
+    // creates a canvas to put the ui on
+    var canvas = new zebra.ui.zCanvas("can");
+    // image for the background
+    var toilet = new zebra.ui.ImagePan("./pics/pic2.png");
+    // play button
+    var play = new zebra.ui.Button(new zebra.ui.ImagePan("./pics/PlayButton.png"));
+    // instructions button
+    var inst = new zebra.ui.Button(new zebra.ui.ImagePan("./pics/InstructionsButton.png"));
+    // basic functionality
+    // *later on need to add sounds to this
+    play.bind(function() {
+        window.alert("Play Pressed");
     });
+    inst.bind(function() {
+        window.alert("Instructions Pressed");
+    });
+    // sets a bland border around the canvas
+    canvas.setBorder(zebra.ui.borders.plain);
+    // Sets the layout to have TOP, CENTER, LEFT, RIGHT, BOTTOM
+    canvas.root.setLayout(new zebra.layout.BorderLayout(8));
+    canvas.root.add(zebra.layout.CENTER, toilet);
+    // Creates a new panel with a new layout inside it called a FlowLayout
+    // This is just to easily flow the buttons onto the page
+    var botP = new zebra.ui.Panel(new zebra.layout.FlowLayout(
+        zebra.layout.CENTER, zebra.layout.BOTTOM, zebra.layout.VERTICAL, 2));
+    // Adds the buttons to the panel
+    botP.add(play);
+    botP.add(inst);
+    // Adds the panel to the page
+    canvas.root.add(zebra.layout.BOTTOM, botP);
+});
--- Testing/test.js
+++ Testing/test.js
@@ -3,9 +3,20 @@ zebra.ready(function() {
     var toilet = new zebra.ui.ImagePan("./pics/pic2.png");
     var play = new zebra.ui.Button(new zebra.ui.ImagePan("./pics/PlayButton.png"));
     var inst = new zebra.ui.Button(new zebra.ui.ImagePan("./pics/InstructionsButton.png"));
+    play.bind(function() {
+        window.alert("Play Pressed");
+    });
+    inst.bind(function() {
+    window.alert("Instructions Pressed");
+    });
     canvas.setBorder(zebra.ui.borders.plain);
     canvas.root.setLayout(new zebra.layout.BorderLayout(8));
+    // Read documentation for the first page, but this is just showing
+    // how the page renders the buttons in order of when they are written
     canvas.root.add(zebra.layout.CENTER, toilet);
-    canvas.root.add(zebra.layout.BOTTOM, play);
+    canvas.root.add(zebra.layout.BOTTOM, new zebra.ui.Button("Loads bottom up in panel"));
     canvas.root.add(zebra.layout.BOTTOM, inst);
+    canvas.root.add(zebra.layout.BOTTOM, new zebra.ui.Button("Showing how things load"));
+    canvas.root.add(zebra.layout.BOTTOM, play);
+    canvas.root.add(zebra.layout.BOTTOM, new zebra.ui.Button("Something Cool"));
 });
