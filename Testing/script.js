ec6ea441eb76ae55ad5e28ee82e074b5767d3957
Spencer Pollock <srepollock@gmail.com>
2015-05-04 23:31:11 -0700

    UI 'Look'

--- Testing/script.js
+++ Testing/script.js
@@ -1,8 +1,11 @@
 zebra.ready(function() {
-	
-          var canvas = new zebra.ui.zCanvas(320, 480);
-          canvas.root.setLayout(new zebra.layout.BorderLayout(8));
-          canvas.root.add(zebra.layout.CENTER, new zebra.ui.TextArea())
-          canvas.root.add(zebra.layout.BOTTOM, new zebra.ui.Button(new zebra.ui.ImagePan("./pics/PlayButton.png")));
-        
-        });
+        var canvas = new zebra.ui.zCanvas("can");
+        canvas.setBorder(zebra.ui.borders.plain);
+        canvas.root.setLayout(new zebra.layout.BorderLayout(8));
+        var toilet = new zebra.ui.ImagePan("./pics/pic2.png");
+        canvas.root.add(zebra.layout.CENTER, toilet);
+        var play = new zebra.ui.Button(new zebra.ui.ImagePan("./pics/PlayButton.png"));
+        var inst = new zebra.ui.Button(new zebra.ui.ImagePan("./pics/InstructionsButton.png"));
+        canvas.root.add(zebra.layout.BOTTOM, play);
+        canvas.root.add(zebra.layout.BOTTOM, inst);
+    });
